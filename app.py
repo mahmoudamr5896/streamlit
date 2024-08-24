05c57063274685cd4617821b37bd209190996431
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-08-24 22:04:28 +0300

    jjj

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from cProfile import label
 import streamlit as st
 import pandas as pd
+from datetime import time
+
 st.title("Hello, Streamlit!")
 st.write("Welcome to your first Streamlit app.")
 df = pd.read_csv("data.csv")
 st.write(df)
-
-from datetime import time
 
 appointment = st.slider(
     "Schedule your appointment:", value=(time(11, 30), time(12, 45))
