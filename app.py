95e594d0e639f7fa27d75840c5ede509d9d1fc8b
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-08-24 20:00:42 +0300

    lll

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+from cProfile import label
 import streamlit as st
 import pandas as pd
 st.title("Hello, Streamlit!")
 st.write("Welcome to your first Streamlit app.")
-df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+df = pd.read_csv("data.csv")
+st.write(df)
+
+from datetime import time
+
+appointment = st.slider(
+    "Schedule your appointment:", value=(time(11, 30), time(12, 45))
+)
+st.write("You're scheduled for:", appointment)
